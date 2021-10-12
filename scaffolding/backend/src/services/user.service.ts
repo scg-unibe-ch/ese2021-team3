96feb001e358e14658a078e7d2dcf8a812c5392b
Florian Meinigg <florian.meinigg@students.unibe.ch>
2021-10-12 17:10:19 +0200

    Added errormessage if mail or username is already used

--- scaffolding/backend/src/services/user.service.ts
+++ scaffolding/backend/src/services/user.service.ts
@@ -2,13 +2,29 @@ import { UserAttributes, User } from '../models/user.model';
 import { LoginResponse, LoginRequest } from '../models/login.model';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import {Op} from 'sequelize';
 
 export class UserService {
 
     public register(user: UserAttributes): Promise<UserAttributes> {
         const saltRounds = 12;
         user.password = bcrypt.hashSync(user.password, saltRounds); // hashes the password, never store passwords as plaintext
-        return User.create(user).then(inserted => Promise.resolve(inserted)).catch(err => Promise.reject(err));
+        return User.findOne({
+            where: {
+                    [Op.or]: [
+                    { userName: user.userName },
+                    { email: user.email }
+                ]
+            }
+        })
+            .then(userDuplicate => {
+                if (userDuplicate == null) { // Creates new user if user doesn't exist.
+                    return User.create(user).then(inserted => Promise.resolve(inserted)).catch(err => Promise.reject(err));
+                } else { // Returns error if user already created
+                    return Promise.reject({error: 'already_exists', message: userDuplicate.userName + ' already exists'});
+                }
+            })
+            .catch(err => Promise.reject({ message: err }));
     }
 
     public login(loginRequestee: LoginRequest): Promise<User | LoginResponse> {
