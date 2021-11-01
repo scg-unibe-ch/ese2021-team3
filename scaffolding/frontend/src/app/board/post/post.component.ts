3adfa2742ed1df22929ba89e7d22d6825a689986
Andy Keller <kelaendi@gmail.com>
2021-11-01 19:21:26 +0100

    Edit the display of posts

--- scaffolding/frontend/src/app/board/post/post.component.ts
+++ scaffolding/frontend/src/app/board/post/post.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { USERS } from 'src/app/mocks/mock-users';
 import { Post } from 'src/app/models/post.model';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-post',
@@ -8,12 +10,22 @@ import { Post } from 'src/app/models/post.model';
 })
 export class PostComponent implements OnInit {
 
+  username = "username";
+  users = USERS;
+
   constructor() { }
 
   @Input()
   post?: Post;
 
   ngOnInit(): void {
+    if (this.post){
+      this.username = this.getUsername(this.post.userId);
+    }
+  }
+
+  getUsername(userId: number){
+    return "username"+userId;
   }
 
 }
--- scaffolding/frontend/src/app/mocks/mock-posts.ts
+++ scaffolding/frontend/src/app/mocks/mock-posts.ts
@@ -6,7 +6,7 @@ export const POSTS: Post[] = [
         userId: 1,
         title: "Post 1",
         text: "Text 1",
-        image: ""
+        image: "Image 1"
     },
 
     {
@@ -14,7 +14,7 @@ export const POSTS: Post[] = [
         userId: 2,
         title: "Post 2",
         text: "Text 2",
-        image: ""
+        image: "Image 2"
     },
 
     {
@@ -22,7 +22,7 @@ export const POSTS: Post[] = [
         userId: 3,
         title: "Post 3",
         text: "Text 3",
-        image: ""
+        image: "Image 3"
     },
 
     {
@@ -30,6 +30,6 @@ export const POSTS: Post[] = [
         userId: 4,
         title: "Post 4",
         text: "Text 4",
-        image: ""
+        image: "Image 4"
     }
 ];
