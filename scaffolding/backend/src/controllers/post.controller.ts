636aa718172bf58cc6afb00fb27217f5da614905
MKulo <91249446+MKulo@users.noreply.github.com>
2021-11-03 14:12:48 +0100

    added get call for posts

--- scaffolding/backend/src/controllers/post.controller.ts
+++ scaffolding/backend/src/controllers/post.controller.ts
@@ -3,11 +3,12 @@ import { Router, Request, Response } from 'express';
 import {PostService} from '../services/post.service';
 import {verifyToken} from '../middlewares/checkAuth';
 import {MulterRequest} from '../models/multerRequest.model';
+import {Post} from '../models/post.model';
 
 const postController: Router = express.Router();
 const postService = new PostService();
 
-postController.use(verifyToken);
+// postController.use(verifyToken);
 
 postController.post('/create',
     (req: Request, res: Response) => {
@@ -19,7 +20,7 @@ postController.post('/create',
     }
 );
 
-postController.post('/:id/edit',
+postController.post('/:id/edit', verifyToken,
     (req: Request, res: Response) => {
         req.body.userId = req.body.tokenPayload.userId;
         req.body.postId = req.params.id;
@@ -29,9 +30,17 @@ postController.post('/:id/edit',
     }
 );
 
-postController.post('/:id/image', (req: MulterRequest, res: Response) => {
+postController.post('/:id/image', verifyToken, (req: MulterRequest, res: Response) => {
     req.body.userId = req.body.tokenPayload.userId;
     postService.addImage(req).then(created => res.send(created)).catch(err => res.status(500).send(err));
 });
+
+postController.get('/get',
+    (req: Request, res: Response) => {
+        Post.findAll()
+            .then(list => res.status(200).send(list))
+            .catch(err => res.status(500).send(err));
+    }
+);
 
 export const PostController: Router = postController;
