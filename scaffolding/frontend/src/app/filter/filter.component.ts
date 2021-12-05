ffa82219d92990e880a58c7f46e1c56e2ff0325a
JanZurb <jan.zurbruegg@students.unibe.ch>
2021-12-06 00:28:25 +0100

    added filtercomponent

---
+++ scaffolding/frontend/src/app/filter/filter.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.css']
+})
+
+export class FilterComponent implements OnInit {
+
+  chosenFilter: string = '';
+
+@Output() messageEvent = new EventEmitter<string>();
+
+  constructor() { }
+
+  sendMessage(){
+    this.messageEvent.emit(this.chosenFilter)
+  }
+
+
+
+  ngOnInit(): void {
+  }
+
+}
